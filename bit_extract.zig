ebbb0640f5e60ff0b79e1ed80930d1a1a938d529
Noah Ryan <nsmryan@gmail.com>
2023-02-05 12:54:57 -0500

    Simple version breaks on smaller integers

--- bit_extract.zig
+++ bit_extract.zig
@@ -21,6 +21,22 @@ pub fn main() void {
     // Crashes with a compile error message about needing a power of 2 byte size.
     //var bits_u3: u3 = 1;
     //std.debug.print("0x{X}\n", .{extractBits(bits_u3, 0, 1)});
+
+    // Pull each 16 bit subsection of the u64 as a test of the simplier version.
+    std.debug.print("0x{X}\n", .{extractBitsSimple(bits, 0, 16)});
+    std.debug.print("0x{X}\n", .{extractBitsSimple(bits, 16, 16)});
+    std.debug.print("0x{X}\n", .{extractBitsSimple(bits, 32, 16)});
+    std.debug.print("0x{X}\n", .{extractBitsSimple(bits, 48, 16)});
+    std.debug.print("0x{X}\n", .{extractBitsSimple(bits, 48, 16)});
+
+    // Try with u32 to show that this works on other integer types, while the simpler
+    // version does not.
+    var bits_u32: u32 = 0x01234567;
+    // This does not compile because the extractBitsSimple version generates
+    // branchs for u6 possibilities and notices that the given u32 does not work
+    // with some of these branchs.
+    //std.debug.print("0x{X}\n", .{extractBitsSimple(bits_u32, 10, 16)});
+    std.debug.print("0x{X}\n", .{extractBits(bits_u32, 10, 16)});
 }
 
 // ctz cannot be used within a function signature as it doesn't accept comptime_int.
@@ -41,6 +57,28 @@ pub fn BitIndexType(comptime BaseType: type) type {
 // This is likely very wasteful, generating a lot of code at a call site, although
 // I haven't done any measurements to see how it effect compilation time..
 pub fn extractBits(bits: anytype, bit_offset: BitIndexType(@TypeOf(bits)), field_width_bits: BitIndexType(@TypeOf(bits))) u64 {
+    @setEvalBranchQuota(64 * 64 * 2);
+
+    const size = @sizeOf(@TypeOf(bits));
+    switch (field_width_bits) {
+        inline else => |field_width| {
+            switch (bit_offset) {
+                inline else => |offset| {
+                    const IntType = std.meta.Int(std.builtin.Signedness.unsigned, field_width);
+                    var ptr: *align(size:offset:size) const IntType = @ptrCast(*align(size:offset:size) const IntType, &bits);
+                    return @intCast(u64, ptr.*);
+                },
+            }
+        },
+    }
+}
+
+// This is would be the better version of this function, but it doesn't work on small 'bits' types.
+// The pointer casts in some of the generated branchs fail at compile time on smaller integer types then u64
+// because they represent offsets outside of the valid range even if the user never hit those branches in a
+// particular callsite. This is a problem with moving data into comptime- it must account for all possible
+// dynamic values.
+pub fn extractBitsSimple(bits: anytype, bit_offset: u6, field_width_bits: u6) u64 {
     @setEvalBranchQuota(64 * 64 * 2);
 
     const size = @sizeOf(@TypeOf(bits));
